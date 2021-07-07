05a60b494dab90f797e897e7b5c24019475d34a9
thompson <alfredthompsonovie@gmail.com>
2021-07-07 01:15:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const addCount = document.querySelector(".addition");
+const reduceCount = document.querySelector(".subtraction");
+const result = document.querySelector(".result");
+let total = 0;
+
+addCount.addEventListener("click", () => {
+    total === 50 ? total: total++;
+    result.innerHTML = total;
+    if (total > 0) {
+        result.style.color = "green";
+    } else if (total === 0) {
+        result.style.color = "gray";
+    } else {
+        result.style.color = "red";
+    }
+});
+reduceCount.addEventListener("click", () => {
+    total === 0 ? total: total--;
+    result.innerHTML = total;
+    if (total > 0) {
+        result.style.color = "green";
+    } else if (total === 0) {
+        result.style.color = "gray";
+    } else {
+        result.style.color = "red";
+    }
+});
